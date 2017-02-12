e043f8a21d473dd2917dea3dd7484cb09d45a813
Atsuto55\Atsuto5 <nyxht500@yahoo.ne.jp>
2017-02-12 16:49:16 +0900

    Add browsing function by webView

--- app/src/main/java/com/example/atsuto5/yahoo_rss_reader_app/WebViewActivity.java
+++ app/src/main/java/com/example/atsuto5/yahoo_rss_reader_app/WebViewActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 /**
@@ -17,11 +19,18 @@ public class WebViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
-        TextView textView = (TextView)findViewById(R.id.textView);
+
+
+        WebView yahooWebView = (WebView)findViewById(R.id.yahooWebView);
+        yahooWebView.setWebViewClient(new WebViewClient(){
+           @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+               return false;
+           }
+        });
+
+        //MainActivityからのIntentを取得
         Intent webViewIntent = getIntent();
-        textView.setText(webViewIntent.getStringExtra(URL_KEY));
+        yahooWebView.loadUrl(webViewIntent.getStringExtra(URL_KEY));
     }
-
-
-
 }
